5b889e33d5f1b2279c6515034b32cd9e1538054a
Yiyun Liu <liuyiyun@seas.upenn.edu>
2025-12-04 00:12:25 -0500

    Fix activate

--- isso/ext/notifications.py
+++ isso/ext/notifications.py
@@ -250,7 +250,7 @@ class Ntfy(object):
         self.notify(f"comment {id} deleted")
 
     def _activate_comment(self, thread, comment):
-        self.notify(f"comment {thread.id}s activated")
+        self.notify(f"comment {thread}s activated")
 
     def notify(self, str):
         response = requests.post(urljoin(self.conf.get("url"),self.conf.get("topic")), data = str.encode(encoding='utf-8'))
